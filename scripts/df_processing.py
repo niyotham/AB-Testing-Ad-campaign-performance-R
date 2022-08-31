05b20ad337d5b0c99ecf47b1b67efd3a6fd59669
niyotham <66441805+niyotham@users.noreply.github.com>
2022-08-31 22:46:34 +0200

    initial commit

---
+++ scripts/df_processing.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+def missing_values_table(df):
+    # Total missing values
+    mis_val = df.isnull().sum()
+
+    # Percentage of missing values
+    mis_val_percent = 100 * df.isnull().sum() / len(df)
+
+    # dtype of missing values
+    mis_val_dtype = df.dtypes
+
+    # Make a table with the results
+    mis_val_table = pd.concat([mis_val, mis_val_percent, mis_val_dtype], axis=1)
+
+    # Rename the columns
+    mis_val_table_ren_columns = mis_val_table.rename(
+    columns = {0 : 'Missing Values', 1 : '% of Total Values', 2: 'Dtype'})
+
+    # Sort the table by percentage of missing descending
+    mis_val_table_ren_columns = mis_val_table_ren_columns[
+        mis_val_table_ren_columns.iloc[:,1] != 0].sort_values(
+    '% of Total Values', ascending=False).round(1)
+
+    # Print some summary information
+    print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"      
+        "There are " + str(mis_val_table_ren_columns.shape[0]) +
+          " columns that have missing values.")
+
+    # Return the dataframe with missing information
+    return mis_val_table_ren_columns
+
+def format_float(value):
+    return f'{value:,.2f}'
+
+def find_agg(df:pd.DataFrame, agg_column:str, agg_metric:str, col_name:str, top:int, order=False )->pd.DataFrame:
+    
+    new_df = df.groupby(agg_column)[agg_column].agg(agg_metric).reset_index(name=col_name).\
+                        sort_values(by=col_name, ascending=order)[:top]
+    
+    return new_df
